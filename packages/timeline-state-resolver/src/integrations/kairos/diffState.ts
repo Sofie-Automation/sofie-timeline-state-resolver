76be594618fdee3dbea5675b0a369b6ae073ffba
Julian Waller <git@julusian.co.uk>
2025-08-12 16:34:18 +0100

    wip: more state

--- packages/timeline-state-resolver/src/integrations/kairos/diffState.ts
+++ packages/timeline-state-resolver/src/integrations/kairos/diffState.ts
@@ -1,8 +1,9 @@
 import type { SomeMappingKairos, Mappings } from 'timeline-state-resolver-types'
+import { MacroActiveState } from 'timeline-state-resolver-types'
 import { KairosStateBuilder, type KairosDeviceState } from './stateBuilder'
 import type { KairosCommandWithContext } from '.'
 // eslint-disable-next-line node/no-missing-import
-import { UpdateSceneLayerObject, UpdateSceneObject } from 'kairos-connection'
+import { UpdateSceneLayerObject, UpdateSceneObject, UpdateAuxObject } from 'kairos-connection'
 import { isEqual } from 'underscore'
 
 export function diffKairosStates(
@@ -22,13 +23,13 @@ export function diffKairosStates(
 	// repected when going between two timed objects. eg baseline: { a: 1, b: 2 }, object A: { a: 3, b: 3 } and object B: { a: 4 }.
 	// when going A -> B, it wouldnt know to set b to 2, because that wouldnt be present in the timeline. Depending what the property is, that may be fine or not.
 
-	// commands.push(...diffSceneSnapshots(oldKairosState.sceneSnapshots, newKairosState.sceneSnapshots))
+	commands.push(...diffSceneSnapshots(oldKairosState.sceneSnapshots, newKairosState.sceneSnapshots))
 	commands.push(...diffScenes(oldKairosState.scenes, newKairosState.scenes))
 	commands.push(...diffSceneLayers(oldKairosState.sceneLayers, newKairosState.sceneLayers))
 
-	// commands.push(...diffAuxes(oldKairosState.aux, newKairosState.aux))
+	commands.push(...diffAuxes(oldKairosState.aux, newKairosState.aux))
 
-	// commands.push(...diffMacros(oldKairosState.macros, newKairosState.macros))
+	commands.push(...diffMacros(oldKairosState.macros, newKairosState.macros))
 
 	// commands.push(...diffClipPlayers(oldKairosState.clipPlayers, newKairosState.clipPlayers))
 	// commands.push(...diffRamRecPlayers(oldKairosState.ramRecPlayers, newKairosState.ramRecPlayers))
@@ -109,6 +110,104 @@ function diffSceneLayers(
 	return commands
 }
 
+function diffSceneSnapshots(
+	oldSceneSnapshots: KairosDeviceState['sceneSnapshots'],
+	newSceneSnapshots: KairosDeviceState['sceneSnapshots']
+): KairosCommandWithContext[] {
+	const commands: KairosCommandWithContext[] = []
+
+	const sceneSnapshotKeys = getAllKeysString(oldSceneSnapshots, newSceneSnapshots)
+	for (const sceneSnapshotKey of sceneSnapshotKeys) {
+		const newSceneSnapshot = newSceneSnapshots[sceneSnapshotKey]
+		const oldSceneSnapshot = oldSceneSnapshots[sceneSnapshotKey]
+
+		const sceneSnapshotRef = newSceneSnapshot?.ref || oldSceneSnapshot?.ref
+		if (!sceneSnapshotRef) continue // No scene snapshot to diff
+
+		// Check if active state changed
+		const oldActive = oldSceneSnapshot?.state.active ?? false
+		const newActive = newSceneSnapshot?.state.active ?? false
+
+		if (oldActive !== newActive) {
+			commands.push({
+				timelineObjId: newSceneSnapshot?.timelineObjIds.join(' & ') ?? '',
+				context: `sceneSnapshotKey=${sceneSnapshotKey} newSceneSnapshot=${!!newSceneSnapshot} oldSceneSnapshot=${!!oldSceneSnapshot}`,
+				command: {
+					type: 'scene-recall-snapshot',
+					ref: sceneSnapshotRef,
+					snapshotName: sceneSnapshotKey,
+					active: newActive,
+				},
+			})
+		}
+	}
+
+	return commands
+}
+
+function diffAuxes(oldAuxes: KairosDeviceState['aux'], newAuxes: KairosDeviceState['aux']): KairosCommandWithContext[] {
+	const commands: KairosCommandWithContext[] = []
+
+	const auxKeys = getAllKeysString(oldAuxes, newAuxes)
+	for (const auxKey of auxKeys) {
+		const newAux = newAuxes[auxKey]
+		const oldAux = oldAuxes[auxKey]
+
+		const auxRef = newAux?.ref || oldAux?.ref
+		if (!auxRef) continue // No aux to diff
+
+		const diff = diffObject<UpdateAuxObject>(oldAux?.state.aux, newAux?.state.aux)
+		if (diff) {
+			commands.push({
+				timelineObjId: newAux?.timelineObjIds.join(' & ') ?? '',
+				context: `auxKey=${auxKey} newAux=${!!newAux} oldAux=${!!oldAux}`,
+				command: {
+					type: 'aux',
+					ref: auxRef,
+					values: diff,
+				},
+			})
+		}
+	}
+
+	return commands
+}
+
+function diffMacros(
+	oldMacros: KairosDeviceState['macros'],
+	newMacros: KairosDeviceState['macros']
+): KairosCommandWithContext[] {
+	const commands: KairosCommandWithContext[] = []
+
+	const macroKeys = getAllKeysString(oldMacros, newMacros)
+	for (const macroKey of macroKeys) {
+		const newMacro = newMacros[macroKey]
+		const oldMacro = oldMacros[macroKey]
+
+		const macroRef = newMacro?.ref || oldMacro?.ref
+		if (!macroRef) continue // No macro to diff
+
+		// Check if active state changed
+		const oldActive = oldMacro?.state.active
+		const newActive = newMacro?.state.active
+
+		if (oldActive !== newActive && newActive !== undefined && newActive !== MacroActiveState.UNCHANGED) {
+			commands.push({
+				timelineObjId: newMacro?.timelineObjIds.join(' & ') ?? '',
+				context: `macroKey=${macroKey} newMacro=${!!newMacro} oldMacro=${!!oldMacro}`,
+				command: {
+					type: 'macro',
+					macroName: macroKey,
+					macroRef: macroRef,
+					active: newActive === MacroActiveState.PLAYING,
+				},
+			})
+		}
+	}
+
+	return commands
+}
+
 function diffObject<T>(oldObj: Partial<T> | undefined, newObj: Partial<T> | undefined): Partial<T> | undefined {
 	if (!newObj) return undefined
 
@@ -139,13 +238,4 @@ function getAllKeysString<V>(
 	const newObj = newObj0 ?? {}
 	const rawKeys = Object.keys(oldObj).concat(Object.keys(newObj))
 	return rawKeys.filter((v, i) => keyIsValid(v, oldObj, newObj) && rawKeys.indexOf(v) === i)
-}
-function getAllKeysNumber<V>(
-	oldObj0: { [key: number]: V } | Array<V> | undefined,
-	newObj0: { [key: number]: V } | Array<V> | undefined
-): number[] {
-	const oldObj = oldObj0 ?? []
-	const newObj = newObj0 ?? []
-	const rawKeys = Object.keys(oldObj).concat(Object.keys(newObj))
-	return rawKeys.filter((v, i) => keyIsValid(v, oldObj, newObj) && rawKeys.indexOf(v) === i).map((v) => parseInt(v, 10))
 }
