883ec0d20d7114146b649d9eb63996c7b778c139
ianshade <ianshade@users.noreply.github.com>
2026-03-20 11:12:41 +0100

    fix(EAV-935): visca protocol issues

--- packages/timeline-state-resolver/src/integrations/viscaOverIP/connection/commands/control/resetSeqNumberCommand.ts
+++ packages/timeline-state-resolver/src/integrations/viscaOverIP/connection/commands/control/resetSeqNumberCommand.ts
@@ -1,8 +1,8 @@
-import { ControlCommand } from '../abstractCommand.js'
+import { ViscaDeviceSettingCommand } from '../abstractCommand.js'
 
-/** The IF_Clear command */
-export class ResetSequenceNumberCommand extends ControlCommand {
+/** The IF_Clear command - VISCA device setting command (type 0x0120), payload 8X 01 00 01 FF where X=1 for VISCA over IP */
+export class ResetSequenceNumberCommand extends ViscaDeviceSettingCommand {
 	serialize() {
-		return Buffer.from([0x88, 0x01, 0x00, 0x01, 0xff])
+		return Buffer.from([0x81, 0x01, 0x00, 0x01, 0xff])
 	}
 }
